017192d7ab2be71d9a7688e75371a49c8f65c0ff
Nathan Pittard <pittardnm@gmail.com>
2022-05-06 11:44:09 -0400

    .

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -7,20 +7,20 @@ import {MOVIES} from './movieObject'
   styleUrls: ['./movies.component.css'],
   template:`
   <div class="moviesContainer">
-  <div class="movieListContainer" *ngFor="let movie of movies">
-    <ul class="movieList">
-      <li class="movieItem">
-        <div class="movieCover">
-          <a href="{{movie.link}}" target="_blank" ><img src="{{movie.cover}}" class="cover" alt=""></a>
-        </div>
-        <div class="movieInformation">
-          <div class="movieTitle">{{movie.title}}</div>
-          <div class="movieSynopsis">{{movie.synopsis}}</div>
-        </div>
-      </li>
-    </ul>
+    <div class="movieListContainer" *ngFor="let movie of movies">
+      <ul class="movieList">
+        <li class="movieItem">
+          <div class="movieCover">
+            <a href="{{movie.link}}" target="_blank" ><img src="{{movie.cover}}" class="cover" alt=""></a>
+          </div>
+          <div class="movieInformation">
+            <div class="movieTitle">{{movie.title}}</div>
+            <div class="movieSynopsis">{{movie.synopsis}}</div>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
-</div>
 `,
 })
 export class MoviesComponent implements OnInit {
